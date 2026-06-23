544682743ee98d770b730f99b22ffeea58f7f0eb
teja15162 <chintala.teja15162@gmail.com>
2026-06-23 23:35:57 +0530

    Enhanced dice roller with dynamic dice support

--- Dice_roller.py
+++ Dice_roller.py
@@ -1,4 +1,3 @@
-# roll one or two dices, visual output, track it how many times 
 import random
 
 def visual(num) :
@@ -15,40 +14,38 @@ def visual(num) :
     else:
         print(" +---------+ \n | *  *  * | \n |         | \n | *  *  * | \n +---------+")
         
+count = []    
 while True:
-    choice = int(input("enter no of dice to roll (1 or 2) : "))
+    ans = input("do you want to roll the dice (yes/no): ").lower()
     
-    if choice in [1,2] :
+    if ans in ["no","n"] :
+        print("dice rolling stopped!")
+        if len(count) == 0:
+            print("no dice rolled!")
+        for i in range(len(count)):
+            print(f"dice {i+1} rolled : {count[i]} times")
         break
-    else:
-        print("please enter 1 or 2 : ")
-        
-ct = 0
-
-while True:
-    s = input("do you want to roll dice (yes/no) : ").lower()
     
-    if s in ["no", "n"] :
-        print("rolling stopped!")
-        print(f"you rolled dice {ct} times")
-        if choice == 2:
-            print(f"you rolled both dice {ct} times")
-        break;
-        
-    elif s in ["yes", "y"] :
-        roll = random.randint(1,6)
-        ct += 1
-        print(f"dice rolled : {roll}")
-        visual(roll)
-        
-    else:
-        print("please enter yes/no")
-        
-    if choice == 2 :
-        if s in ["yes", "y"] :
-            roll2 = random.randint(1,6)
-            print(f"dice2 rolled : {roll2}")
-            visual(roll2)
-            
-        else :
+    elif ans in ["yes","y"] :
+        try :
+            choice = int(input("enter no of dice to roll : "))
+        except ValueError:
+            print("please enter a number!")
             continue
+        
+        if choice <= 0:
+            print("please enter a positive number")
+            continue
+        
+        for i in range(choice):
+            if i < len(count):
+                count[i] += 1
+            else:
+                count.append(1)
+                
+            roll = random.randint(1,6)
+            print(f"dice {i+1} rolled : {roll}")
+            visual(roll)
+    else :
+        print("please enter yes/no : ")
+        
